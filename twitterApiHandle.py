9f2bea650372e6d3b6737009c0acc9fe9224a879
Quentin RIBAC <ribac.quentin@gmail.com>
2018-06-22 21:30:23 +0100

    Update twitterApiHandle.py

--- twitterApiHandle.py
+++ twitterApiHandle.py
@@ -7,10 +7,10 @@ import twitter # github.com/bear/twitter-python --- sudo pip install python-twit
 
 # api connection
 api = twitter.Api(
-    consumer_key = 'ChvsprEQxdVWatFJA6cgDXHfy',
-    consumer_secret = '1GKMb3S2wMWmGT84tdrFHpirvnJHydDbs0w7tCH8QOrtNTlJTU',
-    access_token_key = '965594046176616448-Nr0Dh8FYVxIzgukU97OBh3vM9BaklIg',
-    access_token_secret = 'oFfETSfBdi2d89OnrroeBbXzKhTLCKmSo8YJHWJGVeIFY',
+    consumer_key = '-',
+    consumer_secret = '-',
+    access_token_key = '-',
+    access_token_secret = '-',
     sleep_on_rate_limit = True
 )
 
